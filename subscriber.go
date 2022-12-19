46d55eff8bf01f097705bab70642876a48afa15b
Prasanna Kumar Reddi <prasanna.reddi@apxor.com>
2022-12-19 13:06:14 +0530

    completed a basic implementation of pub-sub model

---
+++ subscriber.go
@@ -0,0 +1,13 @@
+package main
+
+import "fmt"
+
+func subcriber1(message string) {
+	fmt.Println("Subscriber 1 recieved -> " + message)
+}
+func subcriber2(message string) {
+	fmt.Println("Subscriber 2 recieved -> " + message)
+}
+func subcriber3(message string) {
+	fmt.Println("Subscriber 3 recieved -> " + message)
+}
